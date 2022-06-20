f4793764d47130d686e534368649fcb690fe247a
Kristiana Anderson <andersonkristiana@gmail.com>
2022-06-20 17:08:11 -0500

    deployed

---
+++ bin/deploy.sh
@@ -0,0 +1,5 @@
+aws s3 cp ./dist/assets/ s3://yawna-flagrant-web/css/ --cache-control "public, max-age=31536000" --recursive
+aws s3 cp ./dist/favicon.ico s3://yawna-flagrant-web/ --cache-control "public, max-age=86400"
+aws s3 cp ./dist/index.html s3://yawna-flagrant-web/index.html --cache-control "no-cache, no-store, must-revalidate"
+aws s3 cp ./dist/assets/*.css s3://yawna-flagrant-web/assets/ --cache-control "public, max-age=31536000" --content-type "text/css"
+aws s3 cp ./dist/assets/*.js s3://yawna-flagrant-web/assets/ --cache-control "public, max-age=31536000" --content-type "text/javascript"
--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 resource "aws_s3_bucket" "host_bucket" {
-  bucket = "<PICK_A_BUCKET_NAME>"
+  bucket = "yawna-flagrant-web"
   acl    = "private"
 
   policy = data.aws_iam_policy_document.s3_policy.json
@@ -12,7 +12,7 @@ resource "aws_s3_bucket" "host_bucket" {
 data "aws_iam_policy_document" "s3_policy" {
   statement {
     actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.host_bucket}/*"]
+    resources = ["arn:aws:s3:::yawna-flagrant-web/*"]
 
     principals {
       type        = "AWS"
@@ -39,7 +39,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   is_ipv6_enabled     = true
   default_root_object = "index.html"
 
-  aliases = ["<DOMAIN_NAME>"]
+  aliases = ["flagoff.org","www.flagoff.org"]
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
@@ -69,7 +69,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = "<ARN_FOR_CERTIFICATE"
+    acm_certificate_arn = "arn:aws:acm:us-east-1:927640125007:certificate/895063bb-2393-43e9-bcd6-968cf89649a2"
     ssl_support_method  = "sni-only"
   }
 
@@ -89,10 +89,4 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
 }
 
-resource "aws_route53_record" "cdn_cname" {
-  zone_id = "<ROUTE53_ZONE_ID>"
-  name    = "<DOMAIN_NAME>"
-  type    = "CNAME"
-  ttl     = "300"
-  records = ["${aws_cloudfront_distribution.s3_distribution.domain_name}"]
-}
+
